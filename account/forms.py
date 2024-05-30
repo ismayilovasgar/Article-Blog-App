7f9d02d48c2c747fa8a42956961c95eed3e64a8a
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-05-30 10:20:04 +0400

    update_form_register

--- account/forms.py
+++ account/forms.py
@@ -1,13 +1,44 @@
 from django import forms
 
 
-class RegisterFrom(forms.Form):
-    username = forms.CharField(max_length=30, label="Username")
+def field_style():
+    styles_string = " "
+
+    # List of what you want to add to style the field
+    styles_list = [
+        "height: 30px ;",
+        "margin:15px 0px ;",
+        "border-radius:5px ;",
+        "font-size:18px;",
+    ]
+
+    # Converting the list to a string
+    styles_string = styles_string.join(styles_list)
+    # or
+    # styles_string = ' '.join(styles_list)
+
+    return styles_string
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(
+        max_length=30,
+        label="Username",
+        widget=forms.TextInput(attrs={"class": "myfieldclass", "style": field_style()}),
+    )
     password = forms.CharField(
-        max_length=12, label="Password", widget=forms.PasswordInput
+        max_length=12,
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={"class": "myfieldclass", "style": field_style()}
+        ),
     )
     confirm = forms.CharField(
-        max_length=12, label="Password Confirm", widget=forms.PasswordInput
+        max_length=12,
+        label="Password Confirm",
+        widget=forms.PasswordInput(
+            attrs={"class": "myfieldclass", "style": field_style()}
+        ),
     )
 
     def clean(self):
@@ -17,3 +48,10 @@ class RegisterFrom(forms.Form):
 
         if password and username and password != confirm:
             raise forms.ValidationError("Sifreler Uygunlasmir !!!")
+
+        values = {
+            "username": username,
+            "password": password,
+        }
+
+        return values
--- account/views.py
+++ account/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import redirect, render
+from .forms import *
 
 
 # Create your views here.
 def register__view(request):
-    return render(request, "register.html")
+    form = RegisterForm(request.POST or None)
+
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+    context = {"form": form}
+    return render(request, "register.html", context)
 
 
 def login__view(request):
