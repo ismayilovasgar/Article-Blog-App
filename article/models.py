ba2804e626a10de41e8f6188887133d71ded95f0
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-06-03 00:02:21 +0400

    add_ckeditor

--- article/models.py
+++ article/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 
 # Create your models here.
 class Article(models.Model):
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
-    content = models.TextField()
+    # content = models.TextField()
+    content=RichTextField()
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
