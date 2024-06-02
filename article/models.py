57c6b4b89256cd10d451225dcb34cefcbf77bb4a
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-06-03 00:21:55 +0400

    add_Slug_field

--- article/models.py
+++ article/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -7,8 +8,25 @@ class Article(models.Model):
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     # content = models.TextField()
-    content=RichTextField()
+    content = RichTextField()
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.title} | {self.author}"
+
+
+class User(models.Model):
+    username = models.TextField(unique=True, max_length=40)
+    sluq = models.SlugField(
+        blank=True,
+        unique=True,
+        db_index=True,
+        editable=False,
+    )
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.username)
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f"{self.username}"
