1870c4c4806a02f95550f351e49a95a3f51e4582
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-06-10 20:16:43 +0400

    q

--- core/settings.py
+++ core/settings.py
@@ -160,9 +160,9 @@ CKEDITOR_CONFIGS = {
 }
 
 # * Send Message Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'asgar.ismayilov.21@gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "asgar.ismayilov.21@gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = 'mypassword'
+EMAIL_HOST_PASSWORD = "mypassword"
